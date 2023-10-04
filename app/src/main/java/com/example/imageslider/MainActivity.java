4fd718eaf024dedf33e4e4462a15c383961f8407
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-10-04 11:15:21 +0600

    image slide successfully

--- app/src/main/java/com/example/imageslider/MainActivity.java
+++ app/src/main/java/com/example/imageslider/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ImageSlider imageSlider;
+    ArrayList<SlideModel> imageList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageSlider = findViewById(R.id.imgSlider);
+
+
+        imageList.add(new SlideModel(R.drawable.image1, ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.image2, ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.image3, ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.image4, ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.image5, ScaleTypes.CENTER_CROP));
+
+
+
+        imageSlider.setImageList(imageList);
+
     }
 }
